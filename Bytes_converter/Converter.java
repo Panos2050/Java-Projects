9e8f2d33eb179d5f0448b6bfde8cff53e4ccb854
Panagiotis Mpalampanis <Panagiotismpalampaniss@gmail.com>
2025-11-23 15:03:04 +0200

    Deleted java Java_basics

--- Bytes_converter/Converter.java
+++ Bytes_converter/Converter.java
@@ -13,8 +13,21 @@ public class Converter {
         // Convert int to byte
         byte b = (byte) num;
 
+        // 32-bit binary of the origianal int
+        String intBinary = String.format("%32s", Integer.toBinaryString(num)).replace(' ', '0');
+
+        // Convert signed byte to unsigned
+        int byteUnsigned = b & 0xFF;
+
+         // 8-bit binary of the byte
+        String byteBinary = String.format("%8s", Integer.toBinaryString(byteUnsigned)).replace(' ', '0');
+
+        // Display results
+        System.out.println("\n--- RESULTS ---");
         System.out.println("Original int value: " + num);
+        System.out.println("32-bit binary:       " + intBinary);
         System.out.println("Converted byte value: " + b);
+        System.out.println("8-bit binary:        " + byteBinary);
 
         scanner.close();
 
